951742a233c1053c802727ed68d67ef9ab72d276
Olya <dolcheveleno@mail.ru>
2022-07-14 15:44:28 +0300

    zadacha1

---
+++ 001/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+void Zadacha1()
+{
+    Random rand = new Random();
+    int n = rand.Next(10,151);
+    Console.WriteLine(n);
+    if (n<100)
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+    {
+        int Ones = n%10;
+        Console.WriteLine($"Третья цифра: {Ones}");
+    }
+}
+Zadacha1();
+
