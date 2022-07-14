34b0845f6b741e96a84f49144a71f4d23ab7e692
Olya <dolcheveleno@mail.ru>
2022-07-14 16:51:39 +0300

    zadacha2

---
+++ 002/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет
+*/
+void Zadacha2()
+{
+    Random r = new Random();
+    int n = r.Next(1,8);
+    if (n == 1)
+    {
+        Console.WriteLine("Понедельник - НЕ выходной день.");
+    }
+    else if (n == 2)
+    {
+        Console.WriteLine("Вторник - НЕ выходной день.");
+    }
+    else if (n == 3)
+    {
+        Console.WriteLine("Среда - НЕ выходной день.");
+    }
+    else if (n == 4)
+    {
+        Console.WriteLine("Четверг - НЕ выходной день.");
+    }
+    else if (n == 5)
+    {
+        Console.WriteLine("Пятница - НЕ выходной день.");
+    }
+    else if (n == 6)
+    {
+        Console.WriteLine("Суббота - ВЫХОДНОЙ день!");
+    }
+    else if (n == 7)
+    {
+        Console.WriteLine("Воскресенье - ВЫХОДНОЙ день!");
+    }
+}
+Zadacha2();
